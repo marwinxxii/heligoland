102eb6f27be340ecda9fe876b8f174de70322260
Alexey Agapitov <alexey.agapitov@zoho.com>
2026-01-24 23:39:02 +0100

    Initial project setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Heligoland
+Micro language, interpreter and IDE.
+
+## Decisions
+1. Grammar does not support changing values of the variables, so they are treated as immutable after an assignment
+2. `out` operator can output any expression
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+rootProject.name = "heligoland"
+include(":interpreter")
+include(":editor")
